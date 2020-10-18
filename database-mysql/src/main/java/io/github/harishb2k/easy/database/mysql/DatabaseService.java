a8ec78cdfb97eb78914fd49ee128fbbac8ba00a4
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-10-18 17:28:06 +0530

    [x] Added database module to provide DB support

---
+++ database-mysql/src/main/java/io/github/harishb2k/easy/database/mysql/DatabaseService.java
@@ -0,0 +1,13 @@
+package io.github.harishb2k.easy.database.mysql;
+
+import io.github.harishb2k.easy.database.IDatabaseService;
+
+public class DatabaseService implements IDatabaseService {
+    @Override
+    public void startDatabase() {
+    }
+
+    @Override
+    public void stopDatabase() {
+    }
+}
---
+++ database/src/main/java/io/github/harishb2k/easy/database/IDatabaseService.java
@@ -0,0 +1,6 @@
+package io.github.harishb2k.easy.database;
+
+public interface IDatabaseService {
+    void startDatabase();
+    void stopDatabase();
+}
