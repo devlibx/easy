c79b63ce52950eac8de66d0503f110df37dafd76
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-10-21 20:08:59 +0530

    Added 0.0.8

--- dropwizard-app/src/main/java/io/github/harishb2k/easy/app/dropwizard/BaseApplication.java
+++ dropwizard-app/src/main/java/io/github/harishb2k/easy/app/dropwizard/BaseApplication.java
@@ -4,12 +4,27 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseApplication<T extends Configuration> extends Application<T> {
+
+    @Override
+    public void initialize(Bootstrap<T> bootstrap) {
+        super.initialize(bootstrap);
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(
+                        bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+    }
+
     @Override
     public void run(T t, Environment environment) throws Exception {
         environment.getObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
