fb5dd11477b03ae25bbbd44898ca9b7f53289184
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-10-25 03:56:06 +0530

    [maven-release-plugin] rollback the release of 0.0.12

--- release.sh
+++ release.sh
@@ -1,1 +1,2 @@
- mvn -B clean install -Dtag=0.0.12 release:prepare release:perform deploy -P release
+ # mvn -B -DdryRun=true clean install -Dtag=0.0.1 release:prepare release:perform deploy -P release
+ mvn -B -DdryRun=true clean install -Dtag=0.0.13 release:prepare release:perform deploy -P release
