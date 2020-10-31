d3df7344487d172f1c32c79af51a7e0e2f2ef213
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-11-01 04:14:08 +0530

    Marked default IMetrics class - Use NoOpMetrics if not provided

--- helper/src/main/java/io/gitbub/harishb2k/easy/helper/metrics/IMetrics.java
+++ helper/src/main/java/io/gitbub/harishb2k/easy/helper/metrics/IMetrics.java
@@ -1,9 +1,11 @@
 package io.gitbub.harishb2k.easy.helper.metrics;
 
+import com.google.inject.ImplementedBy;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Callable;
 
+@ImplementedBy(IMetrics.NoOpMetrics.class)
 public interface IMetrics {
 
     /**
