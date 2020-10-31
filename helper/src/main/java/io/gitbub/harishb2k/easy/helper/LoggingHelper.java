b9a14420095d2ba90eb467bbb4632edcc9681389
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-11-01 03:45:26 +0530

    Fied duplicate logging

--- helper/src/main/java/io/gitbub/harishb2k/easy/helper/LoggingHelper.java
+++ helper/src/main/java/io/gitbub/harishb2k/easy/helper/LoggingHelper.java
@@ -3,6 +3,9 @@ package io.gitbub.harishb2k.easy.helper;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.apache.log4j.Level.DEBUG;
 import static org.apache.log4j.Level.INFO;
@@ -10,6 +13,7 @@ import static org.apache.log4j.Level.OFF;
 import static org.apache.log4j.Level.TRACE;
 
 public class LoggingHelper {
+    private static final AtomicBoolean initDone = new AtomicBoolean(false);
 
     /**
      * Helper to setup logger
@@ -23,15 +27,20 @@ public class LoggingHelper {
     }
 
     private static void setupLoggingApacheLog4j() {
-        ConsoleAppender console = new ConsoleAppender();
-        // String PATTERN = "%d [%p|%c|%C{1}] %m%n";
-        String PATTERN = "%d [%C{1}] %m%n";
-        console.setLayout(new PatternLayout(PATTERN));
-        console.setThreshold(DEBUG);
-        console.activateOptions();
-        Logger.getRootLogger().addAppender(console);
-        Logger.getRootLogger().setLevel(INFO);
-        Logger.getLogger("io.github.harishb2k.easy.http.sync").setLevel(OFF);
-        Logger.getLogger(LocalHttpServer.class).setLevel(INFO);
+        synchronized (initDone) {
+            if (initDone.get()) return;
+            initDone.set(true);
+
+            ConsoleAppender console = new ConsoleAppender();
+            // String PATTERN = "%d [%p|%c|%C{1}] %m%n";
+            String PATTERN = "%d [%C{1}] %m%n";
+            console.setLayout(new PatternLayout(PATTERN));
+            console.setThreshold(DEBUG);
+            console.activateOptions();
+            // Logger.getRootLogger().addAppender(console);
+            Logger.getRootLogger().setLevel(INFO);
+            Logger.getLogger("io.github.harishb2k.easy.http.sync").setLevel(OFF);
+            Logger.getLogger(LocalHttpServer.class).setLevel(INFO);
+        }
     }
 }
