375028a3d5555cb92952b692fdab721ee3b28dcc
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-11-02 23:19:53 +0530

    Fix build

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
  # mvn -B -DdryRun=true clean install -Dtag=0.0.1 release:prepare release:perform deploy -P release
  # mvn -B clean install -Dtag=0.0.16 release:prepare release:perform deploy -P release
- mvn -B clean install -Dmaven.test.skip=true -DskipTests -Dtag=0.0.28 release:prepare release:perform -P release
+ mvn -B clean install -Dmaven.test.skip=true -DskipTests -Dtag=0.0.29 release:prepare release:perform -P release
