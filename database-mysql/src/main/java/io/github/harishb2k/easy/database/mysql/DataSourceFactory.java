7122a1f2e500fbc0aa9b0006f17d9d8f89d6275b
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-11-07 06:23:07 +0530

    Release locking

--- database-mysql/src/main/java/io/github/harishb2k/easy/database/mysql/DataSourceFactory.java
+++ database-mysql/src/main/java/io/github/harishb2k/easy/database/mysql/DataSourceFactory.java
@@ -1,7 +1,9 @@
 package io.github.harishb2k.easy.database.mysql;
 
+import com.codahale.metrics.MetricRegistry;
 import com.google.common.base.Strings;
 import com.zaxxer.hikari.HikariDataSource;
+import io.gitbub.harishb2k.easy.helper.ApplicationContext;
 import io.github.harishb2k.easy.database.mysql.transaction.TransactionContext;
 import io.github.harishb2k.easy.database.mysql.transaction.TransactionContext.Context;
 import lombok.Getter;
@@ -34,6 +36,10 @@ public class DataSourceFactory {
     }
 
     public void register(DataSource dataSource) {
+
+        // Try to install MetricRegistry if possible
+        installMetricRegistry(dataSource);
+
         synchronized (LOCK) {
             if (transactionAwareDatasource) {
                 dataSourceMap.put(DATASOURCE_DEFAULT, new TransactionAwareDataSourceProxy(dataSource));
@@ -45,6 +51,10 @@ public class DataSourceFactory {
     }
 
     public void register(String dataSourceName, DataSource dataSource) {
+
+        // Try to install MetricRegistry if possible
+        installMetricRegistry(dataSource);
+
         synchronized (LOCK) {
             if (transactionAwareDatasource) {
                 dataSourceMap.put(dataSourceName, new TransactionAwareDataSourceProxy(dataSource));
@@ -101,6 +111,25 @@ public class DataSourceFactory {
 
         if (closed.get() != count) {
             throw new RuntimeException("We had " + count + " datasource registered, but only " + closed.get() + " are closed. This will cause connection leak. Please check.");
+        }
+    }
+
+    private void installMetricRegistry(DataSource dataSource) {
+        try {
+            MetricRegistry metricRegistry = ApplicationContext.getInstance(MetricRegistry.class);
+            if (metricRegistry == null) {
+                log.warn("MetricRegistry is not found - we will not register metric for datasource");
+                return;
+            }
+            if (dataSource instanceof HikariDataSource) {
+                HikariDataSource _dataSource = (HikariDataSource) dataSource;
+                _dataSource.setMetricRegistry(metricRegistry);
+                log.warn("MetricRegistry registered: dataSource={}, metricRegistry={}", _dataSource, metricRegistry);
+            } else {
+                log.warn("MetricRegistry is not found - we will not register metric for datasource (only HikariDataSource is supported)");
+            }
+        } catch (Throwable e) {
+            log.warn("Failed to register metric for datasource: datasource={}", dataSource, e);
         }
     }
 }
