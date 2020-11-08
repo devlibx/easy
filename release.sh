477e4c03315eda8e6eaa26dc111906eedd1b4216
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-11-09 03:58:33 +0530

    Moved to JUnit5

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
  # mvn -B -DdryRun=true clean install -Dtag=0.0.1 release:prepare release:perform deploy -P release
  # mvn -B clean install -Dtag=0.0.16 release:prepare release:perform deploy -P release
- mvn -B clean install -Dmaven.test.skip=true -DskipTests -Dtag=0.0.40 release:prepare release:perform -P release
+ mvn -B clean install -Dmaven.test.skip=true -DskipTests -Dtag=0.0.41 release:prepare release:perform -P release
 
 # Rollback
 # mvn release:rollback
