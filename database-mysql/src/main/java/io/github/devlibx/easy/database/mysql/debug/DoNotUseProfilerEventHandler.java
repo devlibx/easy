4c39fc1a9465960120c05f15774f0ab45b8e5d97
devlibx <harishbohara@harish.local>
2021-04-11 14:02:44 +0530

    using mysql 8.0 driver

--- database-mysql/src/main/java/io/github/devlibx/easy/database/mysql/debug/DoNotUseProfilerEventHandler.java
+++ database-mysql/src/main/java/io/github/devlibx/easy/database/mysql/debug/DoNotUseProfilerEventHandler.java
@@ -1,23 +1,33 @@
 package io.github.devlibx.easy.database.mysql.debug;
 
-import com.mysql.cj.api.ProfilerEvent;
-import com.mysql.cj.api.ProfilerEventHandler;
-import com.mysql.cj.api.log.Log;
+import com.mysql.cj.Query;
+import com.mysql.cj.Session;
+import com.mysql.cj.log.Log;
+import com.mysql.cj.log.ProfilerEvent;
+import com.mysql.cj.log.ProfilerEventHandler;
+import com.mysql.cj.protocol.Resultset;
 import lombok.extern.slf4j.Slf4j;
+
 
 @Deprecated
 @Slf4j
 public class DoNotUseProfilerEventHandler implements ProfilerEventHandler {
     @Override
     public void init(Log log) {
+
     }
 
     @Override
     public void destroy() {
+
     }
 
     @Override
     public void consumeEvent(ProfilerEvent profilerEvent) {
         log.debug("MySQL Profile Event - " + profilerEvent.toString());
+    }
+
+    @Override
+    public void processEvent(byte b, Session session, Query query, Resultset resultset, long l, Throwable throwable, String s) {
     }
 }
