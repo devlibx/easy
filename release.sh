8a4d5abca18928c1ea35664633acda7a7453f313
devlibx <harishbohara@harish.local>
2021-04-11 13:59:24 +0530

    using mysql 8.0 driver

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
  # mvn -B -DdryRun=true clean install -Dtag=0.0.1 release:prepare release:perform deploy -P release
  # mvn -B clean install -Dtag=0.0.16 release:prepare release:perform deploy -P release
- mvn -B clean install -Dmaven.test.skip=true -DskipTests -Dtag=0.0.48  release:prepare release:perform -P release
+ mvn -B clean install -Dmaven.test.skip=true -DskipTests -Dtag=2.0.0  release:prepare release:perform -P release
 
 # Rollback
 # mvn release:rollback
-# git tag -d v0.1.0
-# git push --delete origin v0.1.0
+# git tag -d 2.0.0
+# git push --delete origin 2.0.0
 # Ignore
