cad83d6f496ea35da9dfb8448053673ed9c2507c
Harish Bohara <harish.bohara@gmail.com>
2021-11-17 12:36:07 +0530

    Added a test case to check perf

--- helper/src/test/java/io/gitbub/devlibx/easy/helper/map/StringObjectMapTest.java
+++ helper/src/test/java/io/gitbub/devlibx/easy/helper/map/StringObjectMapTest.java
@@ -1,5 +1,6 @@
 package io.gitbub.devlibx.easy.helper.map;
 
+import io.gitbub.devlibx.easy.helper.json.JsonUtils;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
@@ -90,5 +91,67 @@ public class StringObjectMapTest extends TestCase {
         assertEquals(2, l.size());
         assertEquals("1", l.get(0));
         assertEquals("2", l.get(1));
+    }
+
+    public void testGetWithKeySeparator_2_100_times() {
+        for (int i = 0 ;i < 20; i++) {
+            testGetWithKeySeparator_2();
+        }
+    }
+
+    public void testGetWithKeySeparator_2() {
+        String s = "{\n" +
+                "  \"_id\": \"61923ef95d7d2e1f2c20ebb3\",\n" +
+                "  \"index\": 0,\n" +
+                "  \"guid\": \"74c17e20-7ab5-4b97-8d3a-daf1563f3829\",\n" +
+                "  \"isActive\": false,\n" +
+                "  \"balance\": \"$3,827.04\",\n" +
+                "  \"picture\": \"http://placehold.it/32x32\",\n" +
+                "  \"age\": 25,\n" +
+                "  \"eyeColor\": \"brown\",\n" +
+                "  \"name\": \"Amalia Kane\",\n" +
+                "  \"gender\": \"female\",\n" +
+                "  \"company\": \"ATGEN\",\n" +
+                "  \"email\": \"amaliakane@atgen.com\",\n" +
+                "  \"phone\": \"+1 (819) 400-3482\",\n" +
+                "  \"address\": \"856 Montauk Court, Waumandee, Rhode Island, 5510\",\n" +
+                "  \"about\": \"Qui amet eiusmod ut proident laboris in pariatur officia duis culpa. Ex ipsum id ad aliquip. Elit adipisicing cupidatat magna velit eu qui laborum. Laborum eiusmod irure adipisicing et nostrud. Dolor ea cupidatat dolore aute nostrud nisi ex ipsum.\\r\\n\",\n" +
+                "  \"registered\": \"2019-01-27T07:40:14 -06:-30\",\n" +
+                "  \"latitude\": -41.835071,\n" +
+                "  \"longitude\": 178.600322,\n" +
+                "  \"tags\": [\n" +
+                "    \"est\",\n" +
+                "    \"occaecat\",\n" +
+                "    \"eu\",\n" +
+                "    \"mollit\",\n" +
+                "    \"consectetur\",\n" +
+                "    \"duis\",\n" +
+                "    \"velit\"\n" +
+                "  ],\n" +
+                "  \"friends\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"Sherrie Burks\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\": 1,\n" +
+                "      \"name\": \"Williams Kidd\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\": 2,\n" +
+                "      \"name\": \"Morse Rodgers\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"greeting\": \"Hello, Amalia Kane! You have 4 unread messages.\",\n" +
+                "  \"favoriteFruit\": \"apple\"\n" +
+                "}";
+
+        StringObjectMap  map = JsonUtils.convertAsStringObjectMap(s);
+        long start = System.currentTimeMillis();
+        for (int i = 0 ; i< 1_000_000; i++) {
+            map.path("sub_1.sub_2.int", Object.class);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("===> " + (end - start));
     }
 }
