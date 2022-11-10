a46f397f107e314a816566012f75810575ae8a76
Harish Bohara <harish.bohara@gmail.com>
2022-11-10 15:10:34 +0530

    added joda datetime support in JsonUtils

--- helper/src/test/java/io/gitbub/devlibx/easy/helper/string/StringHelperTest.java
+++ helper/src/test/java/io/gitbub/devlibx/easy/helper/string/StringHelperTest.java
@@ -2,12 +2,14 @@ package io.gitbub.devlibx.easy.helper.string;
 
 import junit.framework.TestCase;
 import lombok.Data;
+import org.joda.time.DateTime;
 
 public class StringHelperTest extends TestCase {
     public void testWriteSting() {
         PojoClass testClass = new PojoClass();
         testClass.setStr("some string");
         testClass.setAnInt(11);
+        testClass.setDateTime(DateTime.now());
         StringHelper stringHelper = new StringHelper();
         assertNotNull(stringHelper.stringify(testClass));
         assertFalse(stringHelper.stringify(testClass).isEmpty());
@@ -18,5 +20,6 @@ public class StringHelperTest extends TestCase {
     public static class PojoClass {
         private String str;
         private int anInt;
+        private DateTime dateTime;
     }
 }
