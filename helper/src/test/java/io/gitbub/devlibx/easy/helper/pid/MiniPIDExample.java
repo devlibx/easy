4bf89527559df599b433ac41d71d794ebc2bdd45
Harish Bohara <harish.bohara@gmail.com>
2022-11-20 01:24:51 +0530

    added pid controller

--- helper/src/test/java/io/gitbub/devlibx/easy/helper/pid/MiniPIDExample.java
+++ helper/src/test/java/io/gitbub/devlibx/easy/helper/pid/MiniPIDExample.java
@@ -3,7 +3,9 @@ package io.gitbub.devlibx.easy.helper.pid;
 public abstract class MiniPIDExample {
     public static void main(String[] args) throws InterruptedException {
         MiniPID miniPID = new MiniPID(0.25, 0.01, 0.4);
-        miniPID.setSetpoint(100);
+        miniPID.setSetpoint(10);
+        miniPID.setMaxIOutput(53);
+        miniPID.setOutputRampRate(2);
         double actual = 0;
         double output = 0;
         double target = 50;
@@ -11,12 +13,10 @@ public abstract class MiniPIDExample {
         System.err.printf("Target\tActual\tOutput\tError\n");
         for (int i = 0; i < 100; i++) {
             output = miniPID.getOutput(actual, target);
-            actual = actual + output;
-
-            //System.out.println("==========================");
-            //System.out.printf("Current: %3.2f , Actual: %3.2f, Error: %3.2f\n",actual, output, (target-actual));
             System.err.printf("%3.2f\t%3.2f\t%3.2f\t%3.2f\n", target, actual, output, (target - actual));
-            Thread.sleep(1000);
+
+            actual = actual + output;
+            Thread.sleep(100);
         }
     }
 }
